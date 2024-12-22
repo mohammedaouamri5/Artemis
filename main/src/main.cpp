06e0991f6cd56264c877e8dda76125cb0a7a3bbf
Amineuonim <154888127+Amineuonim@users.noreply.github.com>
2024-12-22 11:32:47 +0100

    Update main.cpp

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,19 +1,34 @@
-#include "traingel.hpp"
 #include "core/Win.hpp"
-#include "ply.hpp"
+#include "rendering/camera.hpp"
+#include "loaders/plyobject.hpp"
+#include "loaders/shaderloader.hpp"
+#include "rendering/voxeloid.hpp"
+
 #include <GLFW/glfw3.h>
+
 #include <cstdio>
 #include <cstdlib>
+
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 int main() {
+
+  shader_program shader("main/src/shaders/basicvertex.art",
+                        "main/src/shaders/basicfragment.art");
+  plyobject test("assets/untitled.ply");
+  
   CORE::Win *win = new CORE::Win();
-  win->ADD((CORE::Layout *)new PLY("untitled.ply"));
-  // win->ADD((CORE::Layout *)new Traingel());
+  
+  glm::vec4 pos ={0,0,0,1};
   win->INIT();
+  win->ADD((CORE::Layout *)new camera(pos,win->Return_ptr(),win->height,win->width));
+  win->ADD((CORE::Layout *)new voxeloid({0,0,0},test.vao,test.vertices_count));
+
+
   win->RUN();
-  win->CLOSE();
+
+
   return 0;
 }
